9d1f6985c29697653dd83dabc01e5874cc54e054
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-06-23 08:54:45 -0400

    Made functions for basic math operations and tested them

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hi")
+function addNums(num1,num2) {
+  return num1 + num2;
+}
+
+console.log(addNums(1,2));
+
+function subtractNums(num1,num2) {
+  return num1 - num2;
+}
+
+console.log(subtractNums(4,2))
+
+function multiplyNums(num1,num2) {
+  return num1 * num2;
+}
+
+console.log(multiplyNums(2,1))
+
+function divideNums(num1,num2) {
+  return num1 / num2;
+}
+
+console.log(divideNums(8,2));

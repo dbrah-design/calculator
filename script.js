f76980137e167b28f9a9349388bd85fe4e9342ce
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-07-14 08:41:33 -0400

    Added event listeners to all calculator btns except equal sign

--- script.js
+++ script.js
@@ -8,25 +8,23 @@ function addNums(num1,num2) {
   return num1 + num2;
 }
 
-console.log(addNums(1,2));
-
 function subtractNums(num1,num2) {
   return num1 - num2;
 }
-
-console.log(subtractNums(4,2))
 
 function multiplyNums(num1,num2) {
   return num1 * num2;
 }
 
-console.log(multiplyNums(2,1))
-
 function divideNums(num1,num2) {
   return num1 / num2;
 }
 
-console.log(divideNums(8,2));
+calculatorBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.value);
+  })
+})
 
 function operate() {
   const num = 1;
@@ -36,7 +34,6 @@ function operate() {
   return operator;
 }
 
-console.log(operate());
 
 
 
